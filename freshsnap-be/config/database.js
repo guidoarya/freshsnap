97953ece0ac5f7f6b4c31891e9deddaa296fc74e
Guido Aryasatya <73012972+guidoarya@users.noreply.github.com>
2022-05-25 21:39:51 +0700

    update database config

--- freshsnap-be/config/database.js
+++ freshsnap-be/config/database.js
@@ -1,6 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("freshsnap", "root", "", {
+// const database = "id18989744_freshsnap_name";
+// const username = "id18989744_freshsnap_usr";
+// const password = "GUIDO123^abc";
+
+const database = "freshsnap";
+const username = "root";
+const password = "";
+
+const db = new Sequelize(database, username, password, {
   host: "localhost",
   dialect: "mysql",
 });
